b3a2697e9d56132317d202ed0208d65d1305d7d6
xuhaoran <xuhaoran.1996@bytedance.com>
2021-02-06 17:29:51 +0800

    add main process

--- raft/persist.go
+++ raft/persist.go
@@ -1,1 +1,122 @@
 package raft
+
+import (
+	"io"
+	"io/ioutil"
+	"os"
+	"strconv"
+	"sync"
+)
+
+const (
+	StateFile = "state"
+	SnapshotFile = "snapshot"
+)
+
+// 用于持久化节点信息和快照
+type Persist struct {
+	mu sync.RWMutex
+	peerId int
+	stateFile string
+	snapshotFile string
+}
+
+func NewPersist(peerId int) *Persist {
+	return &Persist{
+		mu:           sync.RWMutex{},
+		peerId:       peerId,
+		stateFile:    StateFile + strconv.Itoa(peerId),
+		snapshotFile: SnapshotFile + strconv.Itoa(peerId),
+	}
+}
+
+// 保存状态
+func (ps *Persist) SaveRaftState(state []byte) error {
+	ps.mu.Lock()
+	defer ps.mu.Unlock()
+	return ioutil.WriteFile(ps.stateFile, state, 0770)
+}
+
+// 保存snapshot
+func (ps *Persist) SaveStateAndSnapshot(state, snapshot []byte) error {
+	ps.mu.Lock()
+	defer ps.mu.Unlock()
+
+	err := ioutil.WriteFile(ps.stateFile, state, 0700)
+	err = ioutil.WriteFile(ps.snapshotFile, snapshot, 0700)
+	return err
+}
+
+// 读取持久化文件
+func (ps *Persist) ReadRaftState() ([]byte, error) {
+	ps.mu.RLock()
+	defer ps.mu.RUnlock()
+
+	return ioutil.ReadFile(ps.stateFile)
+}
+
+// 读取快照持久化文件
+func (ps *Persist) ReadSnapshot() ([]byte, error) {
+	ps.mu.RLock()
+	defer ps.mu.RUnlock()
+
+	return ioutil.ReadFile(ps.snapshotFile)
+}
+
+// 计算大小
+func (ps *Persist) ReadStateSize() (int, error) {
+	ps.mu.RLock()
+	defer ps.mu.RUnlock()
+
+	data, err := ioutil.ReadFile(ps.stateFile)
+	if err != nil {
+		return -1, err
+	}
+	return len(data), nil
+}
+func (ps *Persist) ReadSnapshotSize() (int, error) {
+	ps.mu.RLock()
+	defer ps.mu.RUnlock()
+
+	data, err := ioutil.ReadFile(ps.snapshotFile)
+	if err != nil {
+		return -1, err
+	}
+	return len(data), nil
+}
+
+// 文件拷贝
+func (ps *Persist) Copy(i int) (*Persist, error) {
+	ps.mu.Lock()
+	defer ps.mu.Unlock()
+
+	stateFileCopy, snapshotFileCopy := ps.stateFile+"_copy", ps.snapshotFile+"_copy"
+	err := copyFile(ps.stateFile, stateFileCopy)
+	err = copyFile(ps.snapshotFile, snapshotFileCopy)
+	if err != nil {
+		return nil, err
+	}
+
+	p := &Persist{
+		mu:           sync.RWMutex{},
+		peerId:       i,
+		stateFile:    StateFile + strconv.Itoa(i),
+		snapshotFile: SnapshotFile + strconv.Itoa(i),
+	}
+	return p, nil
+}
+
+func copyFile(src, dst string) error {
+	source, err := os.Open(src)
+	if err != nil {
+		return err
+	}
+	defer source.Close()
+	destination, err := os.Create(dst)
+	if err != nil {
+		return err
+	}
+	defer destination.Close()
+	_, err = io.Copy(destination, source)
+	return err
+}
--- raft/raft.go
+++ raft/raft.go
@@ -2,9 +2,13 @@ package raft
 
 import (
 	"context"
+	"github.com/golang/protobuf/proto"
+	"log"
 	"math"
 	"math/rand"
+	"sort"
 	"sync"
+	"sync/atomic"
 	"time"
 )
 
@@ -13,11 +17,11 @@ const (
 	CANDIDATE = "Candidate"
 	FOLLOWER = "Follower"
 	// 心跳时间
-	HeartbeatTerm = 100 * time.Millisecond
+	HeartbeatTime = 100 * time.Millisecond
 	// 选举超时时间 10ms - 500ms
 	ElectionTimeoutBase = 400 * time.Millisecond
 	ElectionTimeoutExtra = 100
-	RpcCallTimeout = HeartbeatTerm
+	RpcCallTimeout = HeartbeatTime
 	StayTerm = -1
 	VoteNil = -1
 	TermNil = -1
@@ -75,6 +79,8 @@ type Raft struct {
 	lastIncludedIndex int64
 	// 该条目的任期号
 	lastIncludedTerm int64
+	// 持久化
+	persist *Persist
 	// 网络是否断开，用于测试，false代表网络链接断开
 	networkDrop bool
 	// 用于测试网络不可靠，true代表网络连接不可靠
@@ -294,6 +300,377 @@ func (rf *Raft) sendRPC(peer int, args interface{}, reply interface{}) bool {
 	return false
 }
 
+// 节点重新上线（新建）
+func NewRaftPeer(me int32, persist *Persist, applyChan chan ApplyMsg, clients []RaftClient) *Raft {
+	// 初始化raft节点
+	rf := &Raft{
+		me: me,
+		mu: sync.RWMutex{},
+		state: FOLLOWER,
+		applyChan: applyChan,
+		stopped: false,
+		conditions: make([]*sync.Cond, len(clients)),
+		persist: persist,
+		currentTerm: 0,
+		votedFor: VoteNil,
+		logs: make([]*LogEntry, 0),
+		nextIndex: make([]int64, len(clients)),
+		matchIndex: make([]int64, len(clients)),
+		peers: clients,
+	}
+	// 重置Logs
+	rf.logs = append(rf.logs, &LogEntry{
+		Term: 0,
+		Command: nil,
+	})
+	// 初始化条件变量
+	for i := 0; i < len(clients); i++ {
+		rf.conditions[i] = sync.NewCond(&rf.mu)
+	}
+	// 初始化选举超时时间
+	timeout := randElectTime()
+	rf.timer = &electionTimer{
+		d: timeout,
+		t: time.NewTimer(timeout),
+	}
+	// 读取持久化信息
+	_ = rf.readPersistedState()
+	// 重置commitIndex和ApplyIndex
+	rf.commitIndex = rf.lastIncludedIndex
+	rf.lastApplied = rf.lastIncludedIndex
+	// 初始化nextIndex
+	for i := range rf.peers {
+		rf.nextIndex[i] = rf.lastIndex() + 1
+	}
+	log.Printf("[Raft] raft peer%v start successfully!\n", me)
+	// 开始运行
+	go rf.runPeer()
+
+	return rf
+}
+
+func (rf *Raft) runPeer() {
+	// 持续监听，如果该节点选举超时，开始拉票
+	for {
+		select {
+		case <-rf.timer.t.C:
+			rf.run()
+		}
+	}
+}
+
+func (rf *Raft) run() {
+	log.Printf("[Raft] raft peer%v become candidate, start request vote!\n", rf.me)
+	rf.mu.Lock()
+	// 开始新的term进行拉选票
+	rf.currentTerm++
+	// 改为Candidate状态
+	rf.state = CANDIDATE
+	// 先将票投给自己
+	rf.votedFor = rf.me
+	// 重置定时器
+	rf.resetElectTimer()
+	_ = rf.persistState()
+	// 构造RequestVote请求
+	index := rf.lastIndex()
+	args := RequestVoteArgs{
+		Term:         rf.currentTerm,
+		CandidateId:  rf.me,
+		LastLogIndex: index,                 //自己日志的最新index
+		LastLogTerm:  rf.getLog(index).Term, //最新日志的Term
+	}
+	rf.mu.Unlock()
+	// 用于接收每一个peer的response
+	replyChan := make(chan RequestVoteReply, len(rf.peers))
+	// 用于等待所有RPC完成，同步信号
+	var wg sync.WaitGroup
+	// 开始给每一个peer发送RequestVote RPC
+	for i := range rf.peers {
+		// 跳过自己
+		if int32(i) == rf.me {
+			continue
+		}
+		wg.Add(1)
+		// 开启协程发送rpc
+		go func(peer int) {
+			defer wg.Done()
+			var reply RequestVoteReply
+			respChan := make(chan struct{})
+			go func() {
+				if rf.sendRPC(peer, &args, &reply) {
+					respChan <- struct{}{}
+				}
+			}()
+			select {
+			// RPC超时
+			case <-time.After(RpcCallTimeout):
+				return
+			case <-respChan:
+				// 收到响应
+				replyChan <- reply
+			}
+		}(i)
+	}
+	// 等待所有RPC完成
+	go func() {
+		wg.Wait()
+		// 关闭channel停止接收数据
+		close(replyChan)
+	}()
+	// 统计replyChan中的reply，收集选票，如果得票超过半数则成为leader，votes初始化为1是因为自己投给自己的一票
+	votes, majority := int32(1), int32(len(rf.peers)/2+1)
+	for rep := range replyChan { // 当上面goroutine完成后会关闭channel，从而退出循环
+		rf.mu.Lock()
+		// 如果对方term更大则自己应转为follower
+		if rep.Term > rf.currentTerm {
+			rf.convertToFollower(rep.Term, VoteNil)
+			rf.mu.Unlock()
+			return
+		}
+		// 检查是否仍处于Candidate状态，或者当前 term是否已经更新
+		if rf.state != CANDIDATE || rep.Term < rf.currentTerm {
+			rf.mu.Unlock()
+			return
+		}
+		// 收集选票，注意原子性
+		if rep.VoteGranted {
+			atomic.AddInt32(&votes, 1)
+		}
+		// 如果选票超过半数，转为leader，不用再等待其他选票了
+		if atomic.LoadInt32(&votes) >= majority {
+			rf.state = LEADER
+			rf.mu.Unlock()
+			log.Printf("[Raft] raft peer%v become Leader successfully！\n", rf.me)
+			// 成为leader后开始给其他Peer追加日志并发出心跳，结束该选举goroutine
+			go rf.startAppendEntries()
+			go rf.startHeartbeat()
+			return
+		}
+		rf.mu.Unlock()
+	}
+	// 如果选举失败，转为follower，等待下一次超时选举
+	rf.mu.Lock()
+	rf.convertToFollower(StayTerm, VoteNil)
+	rf.mu.Unlock()
+}
+
+// 心跳
+func (rf *Raft) startHeartbeat() {
+	// 每隔HeartbeatTime传输信息
+	ch := time.Tick(HeartbeatTime)
+	for {
+		// 确保只有leader才有资格发心跳，否则退出
+		if !rf.isRunningLeader() {
+			return
+		}
+		// 向其他Peer发送心跳
+		for i := range rf.peers {
+			if int32(i) == rf.me {
+				// 注意发出心跳的同时应重置定时器，否则会导致新一轮的选举
+				rf.mu.Lock()
+				rf.resetElectTimer()
+				rf.mu.Unlock()
+				continue
+			}
+			// 通知其他Peer 心跳到达
+			rf.conditions[i].Broadcast()
+		}
+		// 等待下一次心跳时间到来
+		<-ch
+	}
+}
+
+// 日志追加
+func (rf *Raft) startAppendEntries() {
+	for i := range rf.peers {
+		// 跳过自己
+		if int32(i) == rf.me {
+			continue
+		}
+		// 开启新的goroutine来发送AppendEntries RPC
+		go func(peer int) {
+			for {
+				// 只有leader才有资格发送该RPC
+				if !rf.isRunningLeader() {
+					return
+				}
+				rf.mu.Lock()
+				// 等待心跳或新的日志到来，心跳和日志共用该RPC
+				rf.conditions[peer].Wait()
+
+				// 同步到最新的日志，如果是心跳的话就没有日志可发了
+				next := rf.nextIndex[peer]
+
+				// 如果下一个需发送的log落后于上一次快照的位置，说明落后太多，可能断开重连了，此时直接发送快照，加快同步
+				if next <= rf.lastIncludedIndex {
+					log.Printf("next: %v, lastIncludedIndex: %v", next, rf.lastIncludedIndex)
+					rf.startInstallSnapshot(peer)
+					continue
+				}
+				// 构造AppendEntries RPC 参数
+				args := AppendEntriesArgs{
+					Term:         rf.currentTerm,
+					LeaderId:     rf.me,
+					Entries:      nil,
+					LeaderCommit: rf.commitIndex,
+				}
+				// 检查是否有日志未发送
+				if next < rf.logLength() {
+					// 上次同步的日志的位置
+					args.PrevLogIndex = next - 1
+					args.PrevLogTerm = rf.getLog(next - 1).Term
+					// 加入需同步的日志
+					args.Entries = append(args.Entries, rf.logs[rf.subIndex(next):]...)
+				}
+				rf.mu.Unlock()
+
+				var reply AppendEntriesReply
+				respCh := make(chan struct{})
+				// 开启一个新的goroutine发送RPC
+				go func() {
+					//if rf.sendAppendEntries(peer, &args, &reply) {
+					if rf.sendRPC(peer, &args, &reply) {
+						respCh <- struct{}{}
+					}
+				}()
+				// 等待RPC发送结果
+				select {
+				case <-time.After(RpcCallTimeout):
+					// 超时会返回重新发送
+					continue
+				case <-respCh:
+					close(respCh)
+				}
+
+				// 如果日志位置匹配失败，也就是出现了冲突
+				if !reply.Success {
+					rf.mu.Lock()
+					// 检查term
+					if reply.Term > rf.currentTerm {
+						rf.convertToFollower(reply.Term, VoteNil)
+						rf.mu.Unlock()
+						return
+					}
+					//检查自己的状态
+					if rf.state != LEADER || reply.Term < rf.currentTerm {
+						rf.mu.Unlock()
+						return
+					}
+					rf.mu.Unlock()
+
+					// 根据冲突的index，回退，发送最后一条匹配的日志位置后的全部日志
+					if reply.ConflictIndex > 0 {
+						rf.mu.Lock()
+						firstConflict := reply.ConflictIndex
+						if reply.ConflictTerm != TermNil {
+							lastIdx := rf.lastIndex()
+							// 找到匹配的term中的最后一条日志，之后发送后面的全部term的日志
+							for i := rf.addIndex(0); i <= lastIdx; i++ {
+								if rf.getLog(i).Term != reply.ConflictTerm {
+									continue
+								}
+								for i <= lastIdx && rf.getLog(i).Term == reply.ConflictTerm {
+									i++
+								}
+								// 匹配的term中的最后一条日志
+								firstConflict = i
+								break
+							}
+						}
+						// 更新下一次待发送的位置
+						rf.nextIndex[peer] = firstConflict
+						rf.mu.Unlock()
+					}
+					// 跳过，开始重新发送
+					continue
+				}
+
+				// 如果追加成功，更新index
+				if len(args.Entries) > 0 {
+					rf.mu.Lock()
+					// 更新index
+					rf.matchIndex[peer] = args.PrevLogIndex + int64(len(args.Entries))
+					rf.nextIndex[peer] = rf.matchIndex[peer] + 1
+					rf.updateCommitIndex()
+					rf.mu.Unlock()
+				}
+			}
+		}(i)
+	}
+}
+
+// 快照
+func (rf *Raft) startInstallSnapshot(peer int) {
+	// 确保只有leader才可以发送snapshot
+	if rf.state != LEADER || rf.stopped {
+		rf.mu.Unlock()
+		return
+	}
+
+	// 读取快找，构造发送请求
+	data, _ := rf.persist.ReadSnapshot()
+	args := InstallSnapshotArgs{
+		Term:              rf.currentTerm,
+		LeaderId:          rf.me,
+		LastIncludedIndex: rf.lastIncludedIndex,
+		LastIncludedTerm:  rf.lastIncludedTerm,
+		Data:              data,
+	}
+	rf.mu.Unlock()
+	log.Printf("[Raft] raft peer%v start to send InstallSnapshot RPC\n", rf.me)
+
+	// 发送RPC并等待返回结果
+	var reply InstallSnapshotReply
+	respCh := make(chan struct{})
+	go func() {
+		//if rf.sendInstallSnapshot(peer, &args, &reply){
+		if rf.sendRPC(peer, &args, &reply) {
+			respCh <- struct{}{}
+		}
+	}()
+	select {
+	case <-time.After(RpcCallTimeout):
+		return
+	case <-respCh:
+		close(respCh)
+	}
+
+	// 处理返回结果
+	rf.mu.Lock()
+	defer rf.mu.Unlock()
+	// 检查term
+	if reply.Term > rf.currentTerm {
+		rf.convertToFollower(reply.Term, VoteNil)
+		return
+	}
+	// 检查自己的状态
+	if rf.state != LEADER || reply.Term < rf.currentTerm { // curTerm changed already
+		return
+	}
+
+	// 更新index
+	rf.matchIndex[peer] = args.LastIncludedIndex
+	rf.nextIndex[peer] = args.LastIncludedIndex + 1
+}
+
+
+
+// 当半数Peer追加了日志，则可以commit
+func (rf *Raft) updateCommitIndex() {
+	n := len(rf.peers)
+	matched := make([]int64, n)
+	for i := range matched {
+		matched[i] = rf.matchIndex[i]
+	}
+	sort.Slice(matched, func(i, j int) bool {
+		return matched[i] > matched[j]
+	})
+	// 半数以上已经append，则更新commitIndex
+	if N := matched[n/2]; N > rf.commitIndex && rf.getLog(N).Term == rf.currentTerm {
+		rf.commitIndex = N
+		rf.apply()
+	}
+}
 // 重设选举定时器
 func (rf *Raft) resetElectTimer() {
 	rf.timer.d = randElectTime()
@@ -327,6 +704,27 @@ func (rf *Raft) apply() {
 	}
 }
 
+// 读取当前Peer的状态，返回当前term以及是否为leader
+func (rf *Raft) GetState() (int64, bool) {
+	rf.mu.RLock()
+	defer rf.mu.RUnlock()
+	return rf.currentTerm, rf.state == LEADER
+}
+
+// 关闭
+func (rf *Raft) Kill() {
+	rf.mu.Lock()
+	rf.stopped = true
+	rf.mu.Unlock()
+}
+
+func (rf *Raft) logLength() int64 {
+	return rf.lastIndex() + 1
+}
+
+func (rf *Raft) addIndex(i int64) int64 {
+	return rf.lastIncludedIndex + i
+}
 
 // index of logs may be changed for snapshot
 func (rf *Raft) getLog(i int64) LogEntry {
@@ -343,4 +741,48 @@ func (rf *Raft) lastIndex() int64 {
 
 func (rf *Raft) lastTerm() int64 {
 	return rf.logs[len(rf.logs)-1].Term
+}
+// 检查raft是否启动以及是否是leader
+func (rf *Raft) isRunningLeader() bool {
+	rf.mu.RLock()
+	defer rf.mu.RUnlock()
+	return rf.state == LEADER && !rf.stopped
+}
+// 节点信息持久化 存储到持久化文件中
+func (rf *Raft) persistState() error {
+	pb := &State{
+		CurrentTerm:       rf.currentTerm,
+		VotedFor:          rf.votedFor,
+		LastIncludedIndex: rf.lastIncludedIndex,
+		LastIncludedTerm:  rf.lastIncludedTerm,
+		Logs:              rf.logs,
+	}
+	// serialize peer's state by proto
+	data, err := proto.Marshal(pb)
+	if err != nil {
+		return err
+	}
+	// persist data to file
+	return rf.persist.SaveRaftState(data)
+}
+
+// 读取持久化文件
+func (rf *Raft) readPersistedState() error {
+	var pb State
+	// read state from persisted file
+	data, err := rf.persist.ReadRaftState()
+	if err != nil {
+		return err
+	}
+	// proto unmarshal data
+	if err = proto.Unmarshal(data, &pb); err != nil {
+		return err
+	}
+	// set raft peer's state
+	rf.currentTerm = pb.CurrentTerm
+	rf.votedFor = pb.VotedFor
+	rf.lastIncludedIndex = pb.LastIncludedIndex
+	rf.lastIncludedTerm = pb.LastIncludedTerm
+	rf.logs = pb.Logs
+	return nil
 }
